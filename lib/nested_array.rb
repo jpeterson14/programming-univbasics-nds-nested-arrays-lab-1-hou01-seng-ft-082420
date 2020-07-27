e112e780552f49b0806b7870afc23e0e288d6f8b
Jake Peterson <jpeterson0514@gmail.com>
2020-07-27 02:53:13 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,7 +37,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   array_new = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
-  array_new[1][1]
+  assembled_matrix[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
